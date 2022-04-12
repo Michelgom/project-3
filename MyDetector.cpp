3b9db0e73458a73d59f1a27f89b0d4d00f82717a
Michelgom <100556024+Michelgom@users.noreply.github.com>
2022-04-12 10:05:28 -0700

    Create MyDetector.cpp

---
+++ MyDetector.cpp
@@ -0,0 +1,22 @@
+/**
+ * TODO: Complete these class definitions
+ */
+
+/// Starter includes
+#include "MyDetector.hpp"
+#include "MyStack.hpp"
+#include "MyQueue.hpp"
+
+
+//
+namespace CPSC131::PalindromeDetector
+{
+	/// Your welcome
+	MyDetector::MyDetector() {}
+	
+	//
+	bool MyDetector::isPalindrome(std::string s)
+	{
+		
+	}
+}
